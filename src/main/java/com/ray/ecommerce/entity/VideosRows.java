a321a1ce4e1ca280bce517754b07d79b5d8cce20
hades-1997 <70182883+hades-1997@users.noreply.github.com>
2021-12-14 20:10:16 +0700

    entity video cat and video rows
    entity video cat and video rows

--- src/main/java/com/ray/ecommerce/entity/VideosRows.java
+++ src/main/java/com/ray/ecommerce/entity/VideosRows.java
@@ -1,4 +1,81 @@
 package com.ray.ecommerce.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "videos_rows")
+@Getter
+@Setter
 public class VideosRows {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "artist")
+    private String artist;
+
+    @Column(name = "sourceid")
+    private int sourceid;
+
+    @Column(name = "add_time")
+    @CreationTimestamp
+    private Date add_time;
+
+    @Column(name = "status")
+    private int status;
+
+    @Column(name = "archive")
+    private int archive;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "alias")
+    private String alias;
+
+    @Column(name = "hometext")
+    private String hometext;
+
+    @Column(name = "vid_path")
+    private String vid_path;
+
+    @Column(name = "vid_type")
+    private String vid_type;
+
+    @Column(name = "vid_duration")
+    private String vid_duration;
+
+    @Column(name = "homeimgfile")
+    private String homeimgfile;
+
+    @Column(name = "homeimgalt")
+    private String homeimgalt;
+
+    @Column(name = "allowed_comm")
+    private String allowed_comm;
+
+    @Column(name = "allowed_rating")
+    private int allowed_rating;
+
+    @Column(name = "hitstotal")
+    private int hitstotal;
+
+    @Column(name = "total_rating")
+    private int total_rating;
+
+    @Column(name = "click_rating")
+    private int click_rating;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "catid")
+    public VideosCat videosCat;
 }

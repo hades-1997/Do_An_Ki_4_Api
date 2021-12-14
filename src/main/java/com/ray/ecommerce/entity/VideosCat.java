024c1ebeb5a8d53c37cc43f8500f87b3e50592e9
hades-1997 <70182883+hades-1997@users.noreply.github.com>
2021-12-14 17:10:45 +0700

    entity
    update-entity videos_cat id

--- src/main/java/com/ray/ecommerce/entity/VideosCat.java
+++ src/main/java/com/ray/ecommerce/entity/VideosCat.java
@@ -1,15 +1,13 @@
 package com.ray.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-
+@Table(name = "videos_cat")
 public class VideosCat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+
 }
